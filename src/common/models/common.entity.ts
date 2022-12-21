80cd283f2013ae6d30296497c6295f2888dc0870
ali aslani <aliaslani.mm@gmail.com>
2022-12-22 00:39:59 +0330

    Fix: Add Create And Update Columns

--- src/common/models/common.entity.ts
+++ src/common/models/common.entity.ts
@@ -1,8 +1,14 @@
-import { PrimaryGeneratedColumn } from "typeorm";
+import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 export class CommonEntity {
 
         @PrimaryGeneratedColumn()
         id: string
+
+        @UpdateDateColumn()
+        updated_at: string
+
+        @CreateDateColumn()
+        created_at: string
 
 }
